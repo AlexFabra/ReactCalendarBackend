91771dbd41a7eecef438460ca110f3b95ac51c5c
Alejandro Fabra <afabra@nexusgeographics.com>
2022-03-28 13:23:03 +0200

    middleware validarCampos añadido

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ host + /api/auth
 
 const {Router} = require('express');
 const {check} = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
 const {crearUsuario,loginUsuario,revalidarToken} = require('./controllers/auth');
@@ -14,7 +15,8 @@ router.post('/new',
     //middlewares
     check('name','El nombre es obligatorio').not().isEmpty(),
     check('email','El email es obligatorio').isEmail(),
-    check('password','El password debe contener al menos 6 caracteres').isLength({min:6})
+    check('password','El password debe contener al menos 6 caracteres').isLength({min:6}),
+    validarCampos
 ],
 crearUsuario);
 
@@ -22,7 +24,8 @@ router.post('/',
 [
     //middlewares
     check('email','El email es obligatorio').isEmail(),
-    check('password','El password debe contener al menos 6 caracteres').isLength({min:6})
+    check('password','El password debe contener al menos 6 caracteres').isLength({min:6}),
+    validarCampos
 ],
 loginUsuario
 );
--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -1,53 +1,38 @@
-const  {response} = require('express');
-const {validationResult} = require('express-validator');
+const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 
 
-const crearUsuario = (req,res=response)=>{
+const crearUsuario = (req, res = response) => {
 
-    const {name,email,password} = req.body;
-    console.log(name,email,password)
-    //manejo de errores:
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        });
-    }
+    const { name, email, password } = req.body;
+
     res.status(201).json({
-        ok:true,
-        msg:'register',
+        ok: true,
+        msg: 'login',
         name,
         email,
         password
     })
+
 };
 
-const loginUsuario = (req,res=response)=>{
+const loginUsuario = (req, res = response) => {
 
-    const {email,password} = req.body;
-    //manejo de errores:
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        });
-    }
+    const { email, password } = req.body;
 
     res.status(201).json({
-        ok:true,
-        msg:'login',
+        ok: true,
+        msg: 'login',
         email,
         password
     })
 }
 
-const revalidarToken = (req,res=response)=>{
+const revalidarToken = (req, res = response) => {
     res.json({
-        ok:true,
-        msg:'renew'
+        ok: true,
+        msg: 'renew'
     })
 }
 
